b8f4377a37b5eac087b2fa384c0441e9caab133f
Hemang Gupta <hemang2608@gmail.com>
2024-03-11 22:34:19 +0530

    11-03-2024

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+  passLength = 0;
+  useLetter = false;
+  useNumbs = false;
+  useSymbols = false;
+  constructor() {}
+  setPassLength(e: Event) {
+    if (!isNaN(+(e.target as HTMLInputElement).value)) {
+      this.passLength = +(e.target as HTMLInputElement).value;
+    }
+  }
+
+  onSubmit() {
+    const numbers = '1234567890';
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()_+';
+    let validChar = '';
+    if (this.useLetter) {
+      validChar += letters;
+    }
+    if (this.useNumbs) {
+      validChar += numbers;
+    }
+    if (this.useSymbols) {
+      validChar += symbols;
+    }
+    let generatedPass = '';
+    if (validChar) {
+      for (let i = 0; i < this.passLength; i++) {
+        let index = Math.floor(Math.random() * validChar.length);
+        generatedPass += validChar[index];
+        console.log(generatedPass);
+      }
+      this.password = generatedPass;
+    }
+  }
 }
